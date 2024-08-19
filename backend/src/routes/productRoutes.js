377f8559dc8e2ce0e51edd52354cb45452e29bc0
trangiathuan <nakrothfaker@gmail.com>
2024-08-19 11:47:06 +0700

    .

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const routerAPI = express.Router()
-const { getProduct, postCreateProduct, getProductDetail, getProductCategory } = require('../controllers/productController')
+const { getProduct, getProductDetail, getProductCategory } = require('../controllers/productController')
 
 //route product
 routerAPI.get('/product', getProduct)
-routerAPI.post('/product', postCreateProduct)
 routerAPI.get('/product-detail/:id', getProductDetail)
 routerAPI.get('/products-category/:id', getProductCategory)
 
